8a5f2ed860d5eb4d54bf694761253d73b34742ec
francfernandeza-alt <franc.fernandeza@duocuc.cl>
2026-06-09 20:34:11 -0400

    Add files via upload

---
+++ herramientas/src/test/java/com/tool_rental/herramientas/HerramientasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool_rental.herramientas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerramientasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
