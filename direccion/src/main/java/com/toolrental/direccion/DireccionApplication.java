691c1c49567468f00d5e8ae7297610e7be7143a2
nilabrin-cloud <ni.labrin@duocuc.cl>
2026-06-11 20:24:08 -0400

    Add files via upload
    Direccion creado, niko....

---
+++ direccion/src/main/java/com/toolrental/direccion/DireccionApplication.java
@@ -0,0 +1,13 @@
+package com.toolrental.direccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DireccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DireccionApplication.class, args);
+	}
+
+}
