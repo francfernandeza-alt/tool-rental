9f270b331dd9c15cb2d4152ff9bda7172a35d016
javier_olave <ja.olavea@duocuc.cl>
2026-06-17 19:12:42 -0400

    Agregar contenido a Dtos y Services base de reservas

--- reservas/src/main/java/com/tool_rental/reservas/dto/ResenaDTO.java
+++ reservas/src/main/java/com/tool_rental/reservas/dto/ResenaDTO.java
@@ -1,5 +1,17 @@
 package com.tool_rental.reservas.dto;
 
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
 public class ResenaDTO {
 
+    private Integer idResena;
+    private Integer puntuacion;
+    private String comentario;
+    private LocalDate fechaResena;
+    private String rutUsuario;
+    private Integer herramientaId;
+    private Integer reservaId;
 }
--- reservas/src/main/java/com/tool_rental/reservas/dto/ReservaDTO.java
+++ reservas/src/main/java/com/tool_rental/reservas/dto/ReservaDTO.java
@@ -1,5 +1,21 @@
 package com.tool_rental.reservas.dto;
 
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
 public class ReservaDTO {
 
+    private Integer idReserva;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private String estadoReserva;
+    private String rutUsuario;
+
+    private Integer tipoReservaId;
+    private String nombreTipoReserva;
+
+    private Integer metodoPagoId;
+    private String nombreMetodoPago;
 }
