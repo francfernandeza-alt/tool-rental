3b3629b767d2c923149b6c55858c18ecaa5d1192
javier_olave <ja.olavea@duocuc.cl>
2026-06-23 17:28:22 -0400

    feat: añadir a reservas global exception handler

---
+++ reservas/src/main/java/com/tool_rental/reservas/exception/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.tool_rental.reservas.exception;
+
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.tool_rental.reservas.dto.ErrorDTO;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDTO> validaciones(MethodArgumentNotValidException e) {
+        String detalle = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorDTO errorDTO = new ErrorDTO();
+
+        errorDTO.setMensaje("Datos de entrada invalidos.");
+        errorDTO.setDetalle(detalle);
+        errorDTO.setCodigoEstado(HttpStatus.BAD_REQUEST.value());
+        errorDTO.setFecha(LocalDateTime.now());
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDTO> errorGlobal(Exception e) {
+        ErrorDTO errorDTO = new ErrorDTO();
+
+        errorDTO.setMensaje("Error interno en el microservicio de reservas");
+        errorDTO.setDetalle("Ocurrio un problema tecnico durante la operacion");
+        errorDTO.setCodigoEstado(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        errorDTO.setFecha(LocalDateTime.now());
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
